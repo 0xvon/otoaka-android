bddb77f8eb64a0c445921683a3a2ec580b08f0e2
teraomarika <marika.terao.moapp@gmail.com>
2022-02-23 18:46:46 +0900

    mypageswipe

---
+++ app/src/main/java/com/example/otoaka/ui/mypage/MyPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.otoaka.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.otoaka.R
+import com.example.otoaka.ui.mypage.MyProfileDetailFragment
+
+class MyPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        // Return a NEW fragment instance in createFragment(int)
+        val fragment = MyProfileDetailFragment()
+        fragment.arguments = Bundle().apply {
+            // Our object is just an integer :-P
+            putInt(ARG_OBJECT, position + 1)
+        }
+        return fragment
+    }
+}
+
+private const val ARG_OBJECT = "object"
---
+++ app/src/main/java/com/example/otoaka/ui/mypage/MyProfileDetailFragment.kt
@@ -0,0 +1,28 @@
+package com.example.otoaka.ui.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.otoaka.R
+
+class MyProfileDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_my_profile_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
+            val textView: TextView = view.findViewById(R.id.text1)
+            textView.text = getInt(ARG_OBJECT).toString()
+        }
+    }
+    private val ARG_OBJECT = "object"
+}
